3b468f29fd8c8f3232167e3f4bc1c7dc2affb99e
develcrafter <nareumae@gmail.com>
2018-11-23 12:17:42 +0900

    to do list part2 is done

--- cycle_one/todo.js
+++ cycle_one/todo.js
@@ -1,41 +1,57 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
-  toDoInput = toDoForm.querySelector("input"),
-  toDoList = document.querySelector(".js-toDoList");
+  toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
 
-const TODOS_LOCAL_STORAGE = "toDos";
+const TODO_LS = "toDos";
 
-function paintToDo(text) {
-  const li = document.createElement("li");
-  const delBtn = document.createElement("button");
-  delBtn.innerText = "💤";
-  const span = document.createElement("span");
-  span.innerText = text;
-
-  li.appendChild(delBtn);
-  li.appendChild(span);
-
-  toDoList.appendChild(li);
-}
+const toDos = [];
 
 function handleToDoSubmit(event) {
   event.preventDefault();
 
   const currentValue = toDoInput.value;
   paintToDo(currentValue);
-  saveToDos(currentValue);
 
   toDoInput.value = "";
 }
 
-function saveToDos(text) {
-  localStorage.setItem(TODOS_LOCAL_STORAGE, text);
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+
+  delBtn.innerText = "✔";
+  span.innerText = text;
+
+  li.appendChild(delBtn);
+  li.appendChild(span);
+
+  const newId = toDos.length + 1;
+
+  li.id = newId;
+  toDoList.appendChild(li);
+
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODO_LS, JSON.stringify(toDos));
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LOCAL_STORAGE);
+  const loadedToDos = localStorage.getItem(TODO_LS);
 
-  if (toDos !== null) {
-    paintToDo(toDos);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo) {
+      paintToDo(toDo.text);
+    });
   }
 }
 
@@ -47,45 +63,109 @@ function init() {
 init();
 
 // const toDoForm = document.querySelector(".js-toDoForm"),
-//   toDoInput = toDoForm.querySelector("input");
+//   toDoInput = toDoForm.querySelector("input"),
+//   toDoList = document.querySelector(".js-toDoList");
 
-// const toDoList = document.querySelector(".js-toDoList");
+// const TODOS_LOCAL_STORAGE = "toDos";
 
-// const TODO_LOCAL_STORAGE = "currentToDo";
+// const toDos = [];
+
+// function saveToDos() {
+//   localStorage.setItem(TODOS_LOCAL_STORAGE, JSON.stringify(toDos));
+// }
 
 // function paintToDo(text) {
 //   const li = document.createElement("li");
+
 //   const delBtn = document.createElement("button");
-//   delBtn.innerText = "❌";
+//   delBtn.innerText = "💤";
+
 //   const span = document.createElement("span");
 //   span.innerText = text;
 
+//   const newId = toDos.length + 1;
+
 //   li.appendChild(span);
 //   li.appendChild(delBtn);
+//   li.id = newId;
 //   toDoList.appendChild(li);
+
+//   const toDoObj = {
+//     text: text,
+//     id: newId
+//   };
+
+//   toDos.push(toDoObj);
+//   saveToDos();
 // }
 
 // function handleToDoSubmit(event) {
 //   event.preventDefault();
 
 //   const currentValue = toDoInput.value;
-//   localStorage.setItem(TODO_LOCAL_STORAGE, currentValue);
 //   paintToDo(currentValue);
 
 //   toDoInput.value = "";
 // }
 
-// function loadToDo() {
-//   const currentToDo = localStorage.getItem(TODO_LOCAL_STORAGE);
+// function loadToDos() {
+//   const loadedToDos = localStorage.getItem(TODOS_LOCAL_STORAGE);
 
-//   if (currentToDo !== null) {
-//     paintToDo(currentToDo);
+//   if (loadedToDos !== null) {
+//     const parsedToDos = JSON.parse(loadedToDos);
+//     parsedToDos.forEach(function(toDo) {
+//       paintToDo(toDo.text);
+//     });
 //   }
 // }
 
 // function init() {
-//   loadToDo();
+//   loadToDos();
 //   toDoForm.addEventListener("submit", handleToDoSubmit);
 // }
 
 // init();
+
+// // const toDoForm = document.querySelector(".js-toDoForm"),
+// //   toDoInput = toDoForm.querySelector("input");
+
+// // const toDoList = document.querySelector(".js-toDoList");
+
+// // const TODO_LOCAL_STORAGE = "currentToDo";
+
+// // function paintToDo(text) {
+// //   const li = document.createElement("li");
+// //   const delBtn = document.createElement("button");
+// //   delBtn.innerText = "❌";
+// //   const span = document.createElement("span");
+// //   span.innerText = text;
+
+// //   li.appendChild(span);
+// //   li.appendChild(delBtn);
+// //   toDoList.appendChild(li);
+// // }
+
+// // function handleToDoSubmit(event) {
+// //   event.preventDefault();
+
+// //   const currentValue = toDoInput.value;
+// //   localStorage.setItem(TODO_LOCAL_STORAGE, currentValue);
+// //   paintToDo(currentValue);
+
+// //   toDoInput.value = "";
+// // }
+
+// // function loadToDo() {
+// //   const currentToDo = localStorage.getItem(TODO_LOCAL_STORAGE);
+
+// //   if (currentToDo !== null) {
+// //     paintToDo(currentToDo);
+// //   }
+// // }
+
+// // function init() {
+// //   loadToDo();
+// //   toDoForm.addEventListener("submit", handleToDoSubmit);
+// // }
+
+// // init();
