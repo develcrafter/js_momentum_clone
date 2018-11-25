15c35ca8b8e05ba2ad49cdaae7222d993762513a
develcrafter <nareumae@gmail.com>
2018-11-25 11:24:43 +0900

    2nd practice's todo list is done

---
+++ cycle_two/todo.js
@@ -0,0 +1,84 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+let toDos = [];
+
+let delCounter = 0;
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo) {
+      paintToDo(toDo.text);
+    });
+  }
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+
+  toDoInput.value = "";
+}
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
+
+  const span = document.createElement("span");
+  span.innerText = text;
+
+  const newId = toDos.length + delCounter + 1;
+
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  li.id = newId;
+
+  toDoList.appendChild(li);
+
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+  // event.preventDefault();
+
+  const btn = event.target;
+  //   console.log(btn);
+  const li = btn.parentNode;
+
+  toDoList.removeChild(li);
+
+  const cleanToDos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+
+  toDos = cleanToDos;
+  saveToDos();
+
+  delCounter++;
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleToDoSubmit);
+}
+
+init();
